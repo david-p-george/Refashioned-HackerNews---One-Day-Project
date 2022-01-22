9bda5a07cafffd42598515448d2a98214fecd0cf
David George <90811730+david-p-george@users.noreply.github.com>
2022-01-22 09:52:01 +0530

    Updated Github Link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
       </div>
 
       <div className="text-white mt-5 ml-5 pr-2 lg:pr-3 sm:ml-[280px] md:ml-5 lg:ml-[60px] 2xl:ml-[300px]">
-        <a href="to-do">
+        <a href="https://github.com/david-p-george/Refashioned-HackerNews---One-Day-Project">
           // Link to my Github repo
           <AiFillGithub className="w-10 h-10 text-black hover:text-mantis transition duration-500 ease-in" />
         </a>
